98af28984e8957b457d978f0e083948d66f543e1
Wojciech Granicki <wojciech.granicki@uj.edu.pl>
2014-06-13 20:21:55 +0200

    Finally worksgit add -A

--- src/main/java/controllers/ShowMessage_Controller.java
+++ src/main/java/controllers/ShowMessage_Controller.java
@@ -1,17 +1,14 @@
 package controllers;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
- 
-
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.AbstractController;
+ @Controller
  
-public class ShowMessage_Controller extends AbstractController {
+public class ShowMessage_Controller{
 
-	@Override
-	protected ModelAndView handleRequestInternal(HttpServletRequest request,
-			HttpServletResponse response) throws Exception {
-		// TODO Auto-generated method stub
+	@RequestMapping("/welcome")
+	public ModelAndView helloWorld(){
+		 
 		ModelAndView model = new ModelAndView("showMessage");
 		model.addObject("msg", "hello world");
  
