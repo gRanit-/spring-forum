f7f7237c70b8a1ceb99497993662ca593dd03ff3
Wojciech Granicki <wojciechgranicki@me.com>
2014-08-12 11:53:18 +0200

    CollectionUtility changes

--- src/main/java/spring/forum/utils/CollectionUtility.java
+++ src/main/java/spring/forum/utils/CollectionUtility.java
@@ -25,7 +25,7 @@ public class CollectionUtility implements Serializable {
 			this.topicID = topicID;
 		}
 		public String getString() {
-			if(postID==-1)
+			if(postID==new Long(-1))
 				return "t_"+userID+"_"+topicID;
 			else return "p_"+topicID+"_"+postID;
 		}
@@ -39,12 +39,12 @@ public class CollectionUtility implements Serializable {
 		private static final long serialVersionUID = -4790153624805339419L;
 
 		public int compare(Element o1, Element o2) {
-			if (o1.postID == -1) {
-				if (o1.userID - o2.userID == 0) {
+			if (o1.postID == new Long(-1)) {
+				if (o1.userID.longValue() - o2.userID.longValue() == 0) {
 					return  (o1.topicID.compareTo(o2.topicID));
 				} else
 					return o1.userID.compareTo(o2.userID);
-			} else if (o1.topicID - o2.topicID == 0) {
+			} else if (o1.topicID.longValue() - o2.topicID.longValue() == 0) {
 				return o1.postID.compareTo(o2.postID);
 			} else
 				return o1.topicID.compareTo(o2.topicID);
